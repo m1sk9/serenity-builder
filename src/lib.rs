56db26862be94134b45f2d0edf036ee7ed62fe99
Sho Sakuma <me@m1sk9.dev>
2025-10-21 13:07:32 +0900

    feat: Add embed model

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,162 @@
-#![deny(clippy::all)]
+//! [![CI](https://github.com/m1sk9/serenity-embed-builder/actions/workflows/ci.yaml/badge.svg)](https://github.com/m1sk9/serenity-embed-builder/actions/workflows/ci.yaml)
+//! [![Release serenity-embed-builder](https://github.com/m1sk9/serenity-embed-builder/actions/workflows/release.yaml/badge.svg)](https://github.com/m1sk9/serenity-embed-builder/actions/workflows/release.yaml)
+//! [![Apache License 2.0](https://img.shields.io/github/license/m1sk9/babyrite?color=%239944ee)](https://github.com/m1sk9/babyrite/blob/main/LICENSE)
+//!
+//! A Builder to Make Serenity Embeds Easier to Handle.
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![deny(clippy::all)]
+#![allow(dead_code)]
+
+use crate::model::SerenityEmbed;
+use serenity::all::{Colour, CreateEmbed};
+
+mod model;
+
+#[derive(thiserror::Error, Debug)]
+pub enum SerenityEmbedConvertError {
+    #[error("The description exceeds the maximum length of 4096 characters.")]
+    DescriptionTooLong,
+    #[error("The number of fields exceeds the maximum of 25.")]
+    TooManyFields,
+}
+
+impl SerenityEmbed {
+    /// Convert the embedded structure created in Builder into a model usable in Serenity.
+    pub fn convert(&self) -> Result<CreateEmbed, SerenityEmbedConvertError> {
+        let mut embed = serenity::builder::CreateEmbed::default();
+
+        if let Some(title) = &self.title {
+            embed = embed.title(title.text.clone()).url(title.url.clone())
+        }
+
+        if let Some(description) = &self.description {
+            if description.len() >= 4096 {
+                return Err(SerenityEmbedConvertError::DescriptionTooLong);
+            }
+
+            embed = embed.description(description);
+        }
+
+        if let Some(timestamp) = &self.timestamp {
+            embed = embed.timestamp(timestamp);
+        }
+
+        if let Some(color) = &self.color {
+            embed = embed.color(Colour(*color));
+        }
+
+        if let Some(footer_text) = &self.footer_text {
+            let mut footer = serenity::builder::CreateEmbedFooter::new(footer_text);
+            if let Some(icon_url) = &self.footer_icon_url {
+                footer = footer.icon_url(icon_url);
+            }
+            embed = embed.footer(footer);
+        }
+
+        if let Some(image_url) = &self.image_url {
+            embed = embed.image(image_url);
+        }
+
+        if let Some(thumbnail_url) = &self.thumbnail_url {
+            embed = embed.thumbnail(thumbnail_url);
+        }
+
+        if let Some(author_name) = &self.author_name {
+            let mut author = serenity::builder::CreateEmbedAuthor::new(author_name);
+            if let Some(url) = &self.author_url {
+                author = author.url(url);
+            }
+            if let Some(icon_url) = &self.author_icon_url {
+                author = author.icon_url(icon_url);
+            }
+            embed = embed.author(author);
+        }
+
+        if let Some(fields) = &self.fields {
+            if fields.len() > 25 {
+                return Err(SerenityEmbedConvertError::TooManyFields);
+            }
+            // 明示的に (String, String, bool) を作って渡すことで
+            // `Into<String>` の曖昧性（複数の impl による推論失敗）を回避する
+            let mapped = fields
+                .iter()
+                .map(|f| (f.name.clone(), f.value.clone(), f.inline));
+            embed = embed.fields(mapped)
+        }
+
+        Ok(embed)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use serenity::all::Timestamp;
+
     use super::*;
+    use crate::model::{SerenityEmbedField, SerenityEmbedTitle};
+
+    static MOCK_TEXT: &str = "This is a test text.";
+    static MOCK_URL: &str = "https://example.com";
+    static MOCK_TIMESTAMP_STR: &str = "2024-01-01T00:00:00Z";
+    static MOCK_COLOR: u32 = 0xff0000;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_embed_conversion() {
+        let fields = vec![
+            SerenityEmbedField::builder()
+                .name(MOCK_TEXT)
+                .value(MOCK_TEXT)
+                .inline(true)
+                .build(),
+            SerenityEmbedField::builder()
+                .name(MOCK_TEXT)
+                .value(MOCK_TEXT)
+                .build(),
+        ];
+
+        // serenity-embed-builder
+        let mock_embed = SerenityEmbed::builder()
+            .title(
+                SerenityEmbedTitle::builder()
+                    .text(MOCK_TEXT)
+                    .url(MOCK_URL)
+                    .build(),
+            )
+            .description(MOCK_TEXT)
+            .timestamp(Timestamp::parse(MOCK_TIMESTAMP_STR).unwrap())
+            .color(MOCK_COLOR)
+            .footer_text(MOCK_TEXT)
+            .footer_icon_url(MOCK_URL)
+            .image_url(MOCK_URL)
+            .thumbnail_url(MOCK_URL)
+            .author_name(MOCK_TEXT)
+            .author_url(MOCK_URL)
+            .author_icon_url(MOCK_URL)
+            .fields(fields)
+            .build();
+        // serenity
+        let serenity_embed = CreateEmbed::default()
+            .title(MOCK_TEXT)
+            .url(MOCK_URL)
+            .description(MOCK_TEXT)
+            .timestamp(Timestamp::parse(MOCK_TIMESTAMP_STR).unwrap())
+            .color(Colour(MOCK_COLOR))
+            .footer(serenity::builder::CreateEmbedFooter::new(MOCK_TEXT).icon_url(MOCK_URL))
+            .image(MOCK_URL)
+            .thumbnail(MOCK_URL)
+            .author(
+                serenity::builder::CreateEmbedAuthor::new(MOCK_TEXT)
+                    .url(MOCK_URL)
+                    .icon_url(MOCK_URL),
+            )
+            .fields(vec![
+                (MOCK_TEXT.to_string(), MOCK_TEXT.to_string(), true),
+                (MOCK_TEXT.to_string(), MOCK_TEXT.to_string(), false),
+            ]);
+
+        let converted = mock_embed.convert();
+
+        assert!(converted.is_ok());
+        assert_eq!(converted.unwrap(), serenity_embed);
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -3,20 +3,26 @@
 ///
 /// Fundamentally, this structure provides the same functionality as Serenity, but this library explicitly removes values that became unnecessary during optimization as a builder.
 ///
+/// # Structure Values
+///
+/// In Serenity, embeddings are composed of multiple structures such as [serenity::builder::CreateEmbedAuthor].
+///
+/// serenity-embed-builder represents these structures as Triples or Doubles. Please refer to the documentation to determine which values correspond to them.
+///
+/// [What is Triples or Doubles?](https://doc.rust-lang.org/book/ch03-02-data-types.html)
+///
 /// # Values
 ///
-/// The following values have been removed from Serenity:
+/// The following values have been removed from [serenity::model::channel::Embed]:
 ///
 /// - `kind`: The type of embed. Discord currently only supports "rich" embeds, so this field is unnecessary.
 /// - `video`: `rich` embeds do not support video content, so this field is unnecessary.
 /// - `provider`: `rich` embeds do not support provider information, so this field is unnecessary.
-///
-/// [Serenity docs: model/channel/embed](https://docs.rs/serenity/latest/serenity/model/channel/struct.Embed.html)
 #[derive(serde::Deserialize, typed_builder::TypedBuilder, Clone)]
 pub struct SerenityEmbed {
     /// The title of the embed.
     #[builder(default, setter(strip_option, into))]
-    pub title: Option<EmbedTitle>,
+    pub title: Option<SerenityEmbedTitle>,
     /// The description of the embed. (up to 4096 characters)
     #[builder(default, setter(strip_option, into))]
     pub description: Option<String>,
@@ -28,86 +34,62 @@ pub struct SerenityEmbed {
     pub timestamp: Option<serenity::all::Timestamp>,
     /**
      * The color of the embed.
-     * Serenity uses Colour, but Builder only allows direct specification from color codes.
+     * Serenity uses [serenity::model::colour::Colour], but Builder only allows direct specification from color codes.
      * e.g. `0xff0000` for red.
      */
     #[builder(default, setter(strip_option, into))]
     pub color: Option<u32>,
     /// The footer of the embed.
     #[builder(default, setter(strip_option, into))]
-    pub footer: Option<SerenityEmbedFooter>,
+    pub footer_text: Option<String>,
+    /// The footer icon url of the embed.
+    #[builder(default, setter(strip_option, into))]
+    pub footer_icon_url: Option<String>,
     /// The image url of the embed.
     #[builder(default, setter(strip_option, into))]
     pub image_url: Option<String>,
     /// The thumbnail url of the embed.
     #[builder(default, setter(strip_option, into))]
     pub thumbnail_url: Option<String>,
-    /// The author of the embed.
+    /// The author name of the embed.
     #[builder(default, setter(strip_option, into))]
-    pub author: Option<EmbedAuthor>,
+    pub author_name: Option<String>,
+    /// The author url of the embed.
+    #[builder(default, setter(strip_option, into))]
+    pub author_url: Option<String>,
+    /// The author icon url of the embed.
+    #[builder(default, setter(strip_option, into))]
+    pub author_icon_url: Option<String>,
     /// The fields of the embed. (up to 25 fields)
     #[builder(default, setter(strip_option, into))]
     pub fields: Option<Vec<SerenityEmbedField>>,
 }
 
-/// Structure for the title of the embed used by [EmbedBuilder].
+/// Title structures used in [SerenityEmbed].
+///
+/// These structures can be used as Vec (arrays) in [SerenityEmbed] and are internally converted to be handled by [serenity::model::Embed].
 #[derive(serde::Deserialize, typed_builder::TypedBuilder, Clone)]
 pub struct SerenityEmbedTitle {
-    /// The text of the embed title.
-    #[builder(default, setter(strip_option, into))]
-    pub title: Option<String>,
-    /// The url of the embed title.
-    #[builder(default, setter(strip_option, into))]
-    pub url: Option<String>,
+    /// The text of the title.
+    #[builder(default, setter(into))]
+    pub text: String,
+    /// The url of the field.
+    #[builder(default, setter(into))]
+    pub url: String,
 }
 
-/// Type alias for [EmbedTitleBuilder].
-pub type EmbedTitle = SerenityEmbedTitle;
-
-/// Structure for the footer of the embed used by [EmbedBuilder].
-#[derive(serde::Deserialize, typed_builder::TypedBuilder, Clone)]
-pub struct SerenityEmbedFooter {
-    /// The text of the embed footer.
-    #[builder(default, setter(strip_option, into))]
-    pub text: Option<String>,
-    /// The icon url of the embed footer.
-    #[builder(default, setter(strip_option, into))]
-    pub icon_url: Option<String>,
-}
-
-/// Type alias for [EmbedFooterBuilder].
-pub type EmbedFooter = SerenityEmbedFooter;
-
-/// Structure for the author of the embed used by [EmbedBuilder].
-#[derive(serde::Deserialize, typed_builder::TypedBuilder, Clone)]
-pub struct SerenityEmbedAuthor {
-    /// The name of the embed author.
-    #[builder(default, setter(strip_option, into))]
-    pub name: Option<String>,
-    /// The url of the embed author.
-    #[builder(default, setter(strip_option, into))]
-    pub url: Option<String>,
-    /// The icon url of the embed author.
-    #[builder(default, setter(strip_option, into))]
-    pub icon_url: Option<String>,
-}
-
-/// Type alias for [EmbedAuthorBuilder].
-pub type EmbedAuthor = SerenityEmbedAuthor;
-
-/// Structure for the field of the embed used by [EmbedBuilder].
+/// Field structures used in [SerenityEmbed].
+///
+/// These structures can be used as Vec (arrays) in [SerenityEmbed] and are internally converted to be handled by [serenity::model::Embed].
 #[derive(serde::Deserialize, typed_builder::TypedBuilder, Clone)]
 pub struct SerenityEmbedField {
-    /// The name of the embed field.
-    #[builder(default, setter(strip_option, into))]
-    pub name: Option<String>,
-    /// The value of the embed field.
-    #[builder(default, setter(strip_option, into))]
-    pub value: Option<String>,
-    /// Whether the field is inline.
-    #[builder(default, setter(strip_option, into))]
-    pub inline: Option<bool>,
+    /// The name of the field.
+    #[builder(default, setter(into))]
+    pub name: String,
+    /// The value of the field.
+    #[builder(default, setter(into))]
+    pub value: String,
+    /// Whether the field is displayed inline. (default: false)
+    #[builder(default = false, setter(into))]
+    pub inline: bool,
 }
-
-/// Type alias for [EmbedFieldBuilder].
-pub type EmbedField = SerenityEmbedField;
